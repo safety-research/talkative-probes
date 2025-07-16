ab493e2b6112fd96fd3117e8ba20f81d06f01a91
Kit FT <kitfrasertaliente@gmail.com>
2025-07-16 19:49:01 +0000

    hi

--- talkative_autoencoder/lens/evaluation/verbose_samples.py
+++ talkative_autoencoder/lens/evaluation/verbose_samples.py
@@ -857,6 +857,7 @@ def process_and_print_verbose_batch_samples(
                 logits_natural_at_p_batched = None
                 logits_baseline_at_p_batched = None
                 logits_aprime_at_p_batched = None
+                logits_approx_at_p_batched = None
                 top_n_zero_tokens = ["N/A"] * top_n_analysis
                 top_n_orig_A_tokens = ["N/A"] * top_n_analysis
                 top_n_aprime_tokens = ["N/A"] * top_n_analysis
